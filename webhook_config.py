6ec75f0934e97d957162aacb5b721e57a5247cdf
iz-aak <izaak33333@gmail.com>
2026-05-02 00:37:28 +0100

    seperate embed urls

--- webhook_config.py
+++ webhook_config.py
@@ -1,2 +1,3 @@
-WEBHOOK_URL = "https://discord.com/api/webhooks/1499914112145297439/pqh2gI3ag5RtbcdZAvsG_Ft-P_SWVN4v3UksgzJ-a9U2XcDHnDqcHJwDMxEi-w_e2x25"
+HEALTH_WEBHOOK_URL = "https://discord.com/api/webhooks/1499917436689059932/6AZejGMR6fGpL65RkIgviwrFGuIgAQOj1bu4VugFD9CEYHDU08gMRJy27Y43hMlQE9xW"
+LOG_WEBHOOK_URL = "https://discord.com/api/webhooks/1499916775305904188/bpRnpFWvy_gRESVrCIJ-9a3n0IofLxy5fYTJl304qr_Q2DiuKnhasfB3E6qMpU8f_Ucy"
 FLASK_URL = "https://a23749-fdd0.e.jrnm.app/"
