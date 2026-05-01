b0c12f5f52633477a76a08e27511fd77f772a982
iz-aak <izaak33333@gmail.com>
2026-05-02 00:24:16 +0100

    add config

---
+++ webhook_config.py
@@ -0,0 +1,2 @@
+WEBHOOK_URL = "https://discord.com/api/webhooks/1499914112145297439/pqh2gI3ag5RtbcdZAvsG_Ft-P_SWVN4v3UksgzJ-a9U2XcDHnDqcHJwDMxEi-w_e2x25"
+FLASK_URL = "https://a23749-fdd0.e.jrnm.app/"
