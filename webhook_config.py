bc3c22c6cf33b464d7522242f139ec0ac7dc5230
iz-aak <izaak33333@gmail.com>
2026-05-02 00:54:20 +0100

    Clear LOG_WEBHOOK_URL in webhook_config.py
    Removed the LOG_WEBHOOK_URL value for security reasons.

--- webhook_config.py
+++ webhook_config.py
@@ -1,3 +1,3 @@
 HEALTH_WEBHOOK_URL = "https://discord.com/api/webhooks/1499917436689059932/6AZejGMR6fGpL65RkIgviwrFGuIgAQOj1bu4VugFD9CEYHDU08gMRJy27Y43hMlQE9xW"
-LOG_WEBHOOK_URL = "https://discord.com/api/webhooks/1499916775305904188/bpRnpFWvy_gRESVrCIJ-9a3n0IofLxy5fYTJl304qr_Q2DiuKnhasfB3E6qMpU8f_Ucy"
+LOG_WEBHOOK_URL = ""
 FLASK_URL = "https://a23749-fdd0.e.jrnm.app/"
